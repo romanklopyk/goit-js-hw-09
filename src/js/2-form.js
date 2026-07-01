f02e7e801ad329b6e6e52e0372eca077a12c8829
romankhlopyk <romankhlopyk@outlook.com>
2026-07-01 21:24:57 +0300

    add form

---
+++ src/js/2-form.js
@@ -0,0 +1,54 @@
+const formData = {
+    email: '',
+    message: '',
+};
+
+const $form = document.querySelector('#form');
+
+$form.addEventListener('input', (e) => {
+    e.preventDefault();
+    formData[e.target.name] = e.target.value;
+});
+
+$form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (formData.email && formData.message) {
+        window.localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+        console.log(formData);
+        clearFormData();
+        clearLocalStorage();
+        $form.reset();
+    } else {
+        alert('Усі поля обов\'язкові до заповнення');
+    }
+});
+
+function checkLocalStorage() {
+    const rawData = window.localStorage.getItem('feedback-form-state');
+    if (rawData) {
+        const { email, message } = JSON.parse(rawData);
+        formData.email = email;
+        formData.message = message;
+    } else {
+        formData.email = '';
+        formData.message = '';
+    }
+}
+
+checkLocalStorage();
+
+function renderForm() {
+    $form.email.value = formData.email;
+    $form.message.value = formData.message;
+}
+
+renderForm();
+
+function clearFormData() {
+    formData.email = '';
+    formData.message = '';
+}
+
+function clearLocalStorage() {
+    window.localStorage.removeItem('feedback-form-state');
+}
